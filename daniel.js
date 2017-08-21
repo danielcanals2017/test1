4fd1f9d5afecb0d1a203d085d022ba6fcf88d760
danielcanals2017 <daniel.canals@stonybrookmedicine.edu>
2017-08-21 16:34:30 -0400

    typo

--- daniel.js
+++ daniel.js
@@ -1,3 +1,15 @@
-console.js('hello' + Date())
+console.log('hello' + Date())
 
-i= 99
+lala=999
+
+// z = x + y
+
+/* 
+parseFloat(x.value)
+*/
+
+x.size = y.size = 10
+
+x.onkeyup = function(ev){
+    console.log('you pressed his', this.key)
+}

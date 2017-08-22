6fc55219437ba679a1cd32e1fb95b1ac53216605
danielcanals2017 <daniel.canals@stonybrookmedicine.edu>
2017-08-22 11:53:33 -0400

    cbio

--- daniel.js
+++ daniel.js
@@ -7,10 +7,17 @@ lala=999
 /* 
 parseFloat(x.value)
 */
+x.defaultValue = 100
+y.defaultValue = 200
+
+z.value = parseFloat(x.value) + parseFloat(y.value)
 
 x.size = y.size = 10
+
 
 x.onkeyup=y.onkeyup=function(ev){
     //console.log('you pressed x :',this)
     z.textContent=parseFloat(x.value)+parseFloat(y.value)
 }
+
+cbio.get()
